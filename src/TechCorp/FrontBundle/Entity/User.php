aaa9c1c79f2b135f4915facdb95e6980eefaafd2
oumarsig <inoumarng@yahoo.fr>
2017-01-06 01:47:23 +0200

    amis d'un user

--- src/TechCorp/FrontBundle/Entity/User.php
+++ src/TechCorp/FrontBundle/Entity/User.php
@@ -33,11 +33,31 @@ class User //extends BaseUser
     */
     protected $statuses;
 
+    /**
+    * @ORM\ManyToMany(targetEntity="User", inversedBy="friendsWithMe")
+    * @ORM\JoinTable(
+    *     name="friends", 
+    *     joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+    *     inverseJoinColumns={
+    *         @ORM\JoinColumn(name="friend_user_id",
+    *                         referencedColumnName="id")    
+    *        }
+    * )
+    **/
+    private $friends;
+
+    /**
+    * @ORM\ManyToMany(targetEntity="User", mappedBy="friends")
+    */
+    private $friendsWithMe;
+
     public function __construct()
     {
         # code...
         //parent::__construct();
-        $this->statuses = new ArrayCollection();
+        $this->statuses      = new ArrayCollection();
+        $this->friends       = new ArrayCollection();
+        $this->friendsWithMe = new ArrayCollection();
     }
 
     /**
@@ -105,4 +125,93 @@ class User //extends BaseUser
     {
         return $this->statuses;
     }
+
+    /**
+     * Add friends
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friends
+     * @return User
+     */
+    public function addFriend(\TechCorp\FrontBundle\Entity\User $friends)
+    {
+        $this->friends[] = $friends;
+
+        return $this;
+    }
+
+    /**
+     * Remove friends
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friends
+     */
+    public function removeFriend(\TechCorp\FrontBundle\Entity\User $friends)
+    {
+        $this->friends->removeElement($friends);
+    }
+
+    /**
+     * Get friends
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getFriends()
+    {
+        return $this->friends;
+    }
+
+    /**
+     * hasFriend friend
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friend
+     * @return boolean
+     */
+    public function hasFriend(\TechCorp\FrontBundle\Entity\User $friend)
+    {
+        return $this->friends->contains($friend);
+    }
+
+    /**
+     * canAddFriend friend
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friend
+     * @return boolean
+     */
+    public function canAddFriend(\TechCorp\FrontBundle\Entity\User $friend)
+    {
+        return $this != $friend && !$this->hasFriend($friend);
+    }
+
+    /**
+     * Add friendsWithMe
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friendsWithMe
+     * @return User
+     */
+    public function addFriendsWithMe(\TechCorp\FrontBundle\Entity\User $friendsWithMe)
+    {
+        $this->friendsWithMe[] = $friendsWithMe;
+
+        return $this;
+    }
+
+    /**
+     * Remove friendsWithMe
+     *
+     * @param \TechCorp\FrontBundle\Entity\User $friendsWithMe
+     */
+    public function removeFriendsWithMe(\TechCorp\FrontBundle\Entity\User $friendsWithMe)
+    {
+        $this->friendsWithMe->removeElement($friendsWithMe);
+    }
+
+    /**
+     * Get friendsWithMe
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getFriendsWithMe()
+    {
+        return $this->friendsWithMe;
+    }
+    
 }
